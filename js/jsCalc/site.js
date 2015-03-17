d46b05b7eff6a508ce35ad669e6e0a7d8abf1329
Yanze LIU <yanzeliu.france@gmail.com>
2015-03-17 11:39:10 -0700

    updated a flag to test a condition needClear

--- js/jsCalc/site.js
+++ js/jsCalc/site.js
@@ -1,29 +1,58 @@
-$(document).ready(function(){
+  var first;
+  var second;
+  var needClear = false;
 
-  $(".numbers").on("click", function(){
-    var pressedValue = $(this).text();
-    var inputValue = $("#input").val();
-    var res = applyRules(inputValue,pressedValue);
-    $("#input").val(res);
+
+
+  $(document).ready(function(){
+
+    $(".numbers").on("click", function(){
+      var pressedValue = $(this).text();
+      var inputValue = $("#input").val();
+      var res = applyRules(inputValue,pressedValue);
+      $("#input").val(res);
+    });
+
+    $("#AC").on("click", function(){
+      $("#input").val("0")
+    });
+
+
+    $("#plus").on("click",function(){
+     first = $("#input").val();
+     needClear = true;
+   });
+
+    $("#equal").on("click",function(){
+     second = $("#input").val();
+     var res = Number(first) + Number(second);
+     $("#input").val(res);
+   });
+
+
   });
 
-  $("#AC").on("click", function(){
-    $("#input").val("0")
-  });
+  function applyRules(inputValue,pressedValue){
+    //reset input value
+    if(inputValue == "0"){
+      inputValue = "";
+    }
 
-
-});
-
-function applyRules(inputValue,pressedValue){
- if(inputValue == "0"){
-  inputValue = "";
-}
-var res;
-if(inputValue.indexOf(".") != -1 && pressedValue == "."){
-  res = inputValue;
-}
-else{
-  res = inputValue + pressedValue;
-}
+  // use only once dot
+  var res;
+  if(inputValue.indexOf(".") != -1 && pressedValue == "."){
+    res = inputValue;
+  }
+  else{
+    res = inputValue + pressedValue;
+  }
+  //
+  if(needClear){
+    needClear = false;
+      res = pressedValue;
+  }
   return res;
+
+
+
 };
