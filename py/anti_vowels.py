b148bf0c6f4ca7d997e8c8e6b71b417c27f447b9
Yanze LIU <yanzeliu.france@gmail.com>
2015-05-23 10:57:14 -0700

    Update anti_vowels.py

--- py/anti_vowels.py
+++ py/anti_vowels.py
@@ -1,5 +1,5 @@
 def anti_vowel(text):
-  for ch in text:
-    if ch in "aeiouAEIOU":
-      text = text.replace(ch,"")
-      return text
+    for ch in text:
+        if ch in "aeiouAEIOU":
+            text = text.replace(ch,"")
+    return text
