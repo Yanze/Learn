bb5e2835559dcd1c8590e90d00e1606fc19b737c
Yanze LIU <yanzeliu.france@gmail.com>
2015-06-08 23:13:57 +0800

    add remove groups

--- py3/restau_management/run.py
+++ py3/restau_management/run.py
@@ -10,7 +10,8 @@ restau = shell.create_new_restau()
 
 cpt = 0
 while restau is None and cpt < restau_creation_max_tries:
-  print("Error in Inputs, please try again ({} tries left)".format(restau_creation_max_tries-1-cpt))
+  print("Error in Inputs, please try again ({} tries left)"
+        .format(restau_creation_max_tries-1-cpt))
   restau = shell.create_new_restau()
   cpt += 1
   if cpt == restau_creation_max_tries-1:
@@ -22,17 +23,23 @@ while restau is None and cpt < restau_creation_max_tries:
 shell.log('New restaurant created{}'.format(restau))
 
 restau.load_menu()
-shell.log('Menu loaded (total dishes: {})'.format(len(restau.menu.dishes)))
+shell.log('Menu loaded (total dishes: {})'
+          .format(len(restau.menu.dishes)))
 
 user_answer = shell.show_main_menu()
-while user_answer != '3':
+while user_answer != '6':
+
   if user_answer == '1':
     shell.show_dishes(restau.menu.dishes)
-
-
   elif user_answer == '2':
     restau.load_menu()
     shell.show_dishes(restau.menu.dishes)
+  elif user_answer == '3':
+    shell.assign_new_table(restau)
+  elif user_answer == '4':
+    shell.show_customer_groups(restau.customer_groups)
+  elif user_answer == '5':
+    shell.release_table(restau)
 
   user_answer = shell.show_main_menu()
 
--- py3/restau_management/shell.py
+++ py3/restau_management/shell.py
@@ -29,7 +29,10 @@ class Shell():
     print('')
     print('1. Show Dishes')
     print('2. Reload & Show Dishes')
-    print('3. Quit Application')
+    print('3. Assign New Table')
+    print('4. Show Customer Groups')
+    print('5. Release Table')
+    print('6. Quit Application')
     print('')
     return input('selection:')
 
@@ -45,3 +48,52 @@ class Shell():
 
   def log(self, msg):
     print('** '+ msg)
+
+  def assign_new_table(self, restau):
+    customers = input('Please enter customer number:')
+    tables = restau.new_customer_group(int(customers))
+    if tables is None:
+      print('Sorry, not enough available tables.')
+    else:
+      print('Tables {} are available for this customer group.'
+          .format(', '.join(map(str, tables))))
+
+  def show_customer_groups(self, groups):
+    print('')
+    print('         All Customer Groups in Restaurant ')
+    print('       =====================================')
+    print('')
+    if len(groups) == 0:
+      print('No customer groups at the moment.')
+
+    for group in groups:
+
+      tables_nb = ""
+      for table in group.tables:
+        tables_nb += str(table.number) + ','
+      tables_nb = tables_nb[:-1]
+      print('Group {} at {} tables, {} customers.'
+            .format(group.id,
+                    tables_nb,
+                    group.number))
+
+  def release_table(self, restau):
+    leaving_customer_group = input('Please enter leaving group id:')
+    group_leaving = None
+
+    for group in restau.customer_groups:
+      if group.id == int(leaving_customer_group):
+        group_leaving = group
+        break
+
+    if group_leaving is None:
+      print("No Group {} found".format(leaving_customer_group))
+    else:
+      restau.customer_group_leave(group_leaving)
+      tables_nb = ""
+      for table in group_leaving.tables:
+        tables_nb += str(table.number) + ','
+      tables_nb = tables_nb[:-1]
+      print('Tables {} are released'.format(tables_nb))
+
+
