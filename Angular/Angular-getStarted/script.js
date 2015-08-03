1ee4cebd14c99132651e3adef3d71273871c98c5
Yanze LIU <yanzeliu.france@gmail.com>
2015-08-03 13:32:59 -0700

    get user's input

--- Angular/Angular-getStarted/script.js
+++ Angular/Angular-getStarted/script.js
@@ -1,25 +1,28 @@
 // Code goes here
-(function() { //avoid global namesapce. 
-    
-var app = angular.module("githubViewer", []); 
-// to create an module. [] is defining an module,  
-// otherwise is asking Angular to get a reference to a module called "githubViewer";  
+(function() { //avoid global namesapce.
+
+var app = angular.module("githubViewer", []);
+// to create an module. [] is defining an module,
+// otherwise is asking Angular to get a reference to a module called "githubViewer";
 
 var MainController = function($scope, $http){
     var onUserComplete = function(response){
         $scope.user = response.data;
     };
-    
+
     var onError = function(reason){
         $scope.error = "Could not fetch the user";
     };
-    
-    $http.get("https://api.github.com/users/perseghini")
-         .then(onUserComplete, onError); // you can also make inline function.
-    
-    $scope.message = "Hello, Angular";
-    
+
+    $scope.search = function(username){
+      $http.get("https://api.github.com/users/" + username)
+         .then(onUserComplete, onError);
+    }
+           
+    $scope.username = "Angular";
+    $scope.message = "GitHub Viewer";
+
 };
 
-    app.controller("MainController", MainController); // registe the controller in module.
+    app.controller("MainController", ["$scope" ,"$http",MainController]); // registe the controller in module.
 }());
